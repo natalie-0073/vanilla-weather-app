28a034be62373249d31fe7e8c4497880461582f6
Nataliia Chala <hatalochka.rv@gmail.com>
2022-06-05 16:10:41 +0100

    updated date so that now you see current time

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+function formatDate() {
+    let date = new Date();
+    let num = date.getDate();
+    let month = date.getMonth();
+    let months = ["January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ];
+    let day = date.getDay();
+    let hours = date.getHours();
+    let minutes = (date.getMinutes() < 10 ? "0" : "") + date.getMinutes();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    return `${num} ${months[month]}, ${days[day]} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
     let currTemp = document.querySelector("#current-temperature");
     currTemp.innerHTML = Math.round(response.data.main.temp);
@@ -7,6 +31,8 @@ function displayTemperature(response) {
     humidity.innerHTML = response.data.main.humidity;
     let windSpeed = document.querySelector("#wind-speed");
     windSpeed.innerHTML = Math.round(response.data.wind.speed);
+    let date = document.querySelector("#date");
+    date.innerHTML = formatDate();
     console.log(response.data);
 }
 let apiKey = "ef6474d7c06b8fcbb3388c0963600854";
